6f218950586680b91e94b54f7c6be290abbe348a
shanye <shanye.xzq@alibaba-inc.com>
2025-03-14 10:36:11 +0800

    examples/complete: improve the examples

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,19 @@
 output "this_ecs_id" {
-  value = alicloud_instance.default.id
+  description = "The ID of the ECS instance."
+  value       = alicloud_instance.default.id
 }
 
 output "this_ecs_name" {
-  value = alicloud_instance.default.instance_name
+  description = "The name of the ECS instance."
+  value       = alicloud_instance.default.instance_name
 }
 
 output "this_rds_id" {
-  value = alicloud_db_instance.default.id
+  description = "The ID of the RDS instance."
+  value       = alicloud_db_instance.default.id
 }
 
 output "this_rds_name" {
-  value = alicloud_db_instance.default.instance_name
+  description = "The name of the RDS instance."
+  value       = alicloud_db_instance.default.instance_name
 }
